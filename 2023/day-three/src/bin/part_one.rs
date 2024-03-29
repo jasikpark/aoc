be977f2f1540d2ba93bd7ce6ea20be42d7609ede
Caleb Jasik <calebjasik@jasik.xyz>
2024-03-28 23:57:11 -0500

    day 3 scaffolding

---
+++ 2023/day-three/src/bin/part_one.rs
@@ -0,0 +1,25 @@
+fn main() {
+    todo!()
+}
+
+pub fn part_one(input: &str) -> usize {
+    todo!()
+}
+
+mod test {
+    #[test]
+    fn test_example() {
+        use crate::part_one;
+        let input = r#"467..114..
+        ...*......
+        ..35..633.
+        ......#...
+        617*......
+        .....+.58.
+        ..592.....
+        ......755.
+        ...$.*....
+        .664.598.."#;
+        assert_eq!(part_one(input), 4361);
+    }
+}
