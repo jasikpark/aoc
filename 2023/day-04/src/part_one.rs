24e191d2599463c89b33d10aa17f1716252cb987
Caleb Jasik <calebjasik@jasik.xyz>
2024-04-08 17:13:11 -0500

    day 4 part one done!

--- 2023/day-04/src/part_one.rs
+++ 2023/day-04/src/part_one.rs
@@ -2,7 +2,7 @@ use nom::{
     bytes::complete::tag,
     character::complete::digit1,
     combinator::map_res,
-    multi::{many1, separated_list1},
+    multi::{many0, many1, separated_list1},
     sequence::tuple,
     IResult,
 };
@@ -11,6 +11,7 @@ fn digit_parser(input: &str) -> IResult<&str, usize> {
     map_res(digit1, str::parse)(input)
 }
 
+#[derive(Debug)]
 pub struct ScratchoffCard {
     card_num: usize,
     /// The first correct number scores 1 point, then each after that doubles the score.
@@ -21,11 +22,11 @@ pub struct ScratchoffCard {
 
 impl ScratchoffCard {
     fn parse(input: &str) -> IResult<&str, Self> {
-        let (input, _) = tuple((tag("Card"), many1(tag(" "))))(input)?;
+        let (input, _) = tuple((many0(tag(" ")), tag("Card"), many1(tag(" "))))(input)?;
         let (input, card_num) = digit_parser(input)?;
         let (input, _) = tag(": ")(input)?;
         let (input, lottery_numbers) = separated_list1(
-            tuple((many1(tag(" ")), tag("|"), many1(tag(" ")))),
+            tuple((many0(tag(" ")), tag("|"), many0(tag(" ")))),
             parse_lottery_numbers,
         )(input)?;
 
@@ -36,7 +37,7 @@ impl ScratchoffCard {
 
         let my_winning_numbers_count = my_numbers
             .iter()
-            .filter(|num| winning_numbers.contains(num))
+            .filter(|num| winning_numbers.iter().any(|winner| winner == *num))
             .count();
 
         let score = Self::calc_score(my_winning_numbers_count);
@@ -81,17 +82,28 @@ type LotteryNums = Vec<usize>;
 
 /// ```
 /// use day_4::part_one::parse_lottery_numbers;
-/// let input = "1 2 3 14 69 11";
+/// let input = " 1 2 3 14 69 11";
 /// assert_eq!(parse_lottery_numbers(input).unwrap(), ("", vec![1, 2, 3, 14, 69, 11]));
 /// ```
 pub fn parse_lottery_numbers(input: &str) -> IResult<&str, LotteryNums> {
-    separated_list1(many1(tag(" ")), digit_parser)(input)
+    let (input, (_, nums, _)) = tuple((
+        many0(tag(" ")),
+        separated_list1(many1(tag(" ")), digit_parser),
+        many0(tag(" ")),
+    ))(input)?;
+
+    Ok((input, nums))
 }
 
 pub fn part_one(input: &str) -> usize {
     input
         .lines()
-        .flat_map(|line| ScratchoffCard::parse(line).map(|card| card.1.score))
+        .flat_map(|line| {
+            ScratchoffCard::parse(line).map(|(input, card)| {
+                assert!(input.is_empty());
+                card.score
+            })
+        })
         .sum()
 }
 
@@ -105,13 +117,13 @@ mod test {
         Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
         Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
         Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"#;
-        assert_eq!(part_one(input), 8);
+        assert_eq!(part_one(input), 13);
     }
 
     #[test]
     fn provided_input() {
         use crate::part_one::part_one;
         let input = include_str!("./input.txt");
-        assert_eq!(part_one(input), 8);
+        assert_eq!(part_one(input), 25004);
     }
 }
